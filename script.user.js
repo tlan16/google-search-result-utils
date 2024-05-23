e8bb4cbd70935d0c7dcf9f7aba705d97b800bfa5
Frank Lan <franklan118@gmail.com>
2024-05-23 04:06:24 +0000

    Create  script.user.js

---
+++ script.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Google Search Result Utils
+// @namespace    https://github.com/tlan16/google-search-result-utils
+// @version      1.0.0
+// @description  Modifies google search result links
+// @author       Frank Lan
+// @match        https://www.google.com.au/*
+// @match        https://www.google.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com.au
+// @run-at      document-idle
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const setNewTab = () => {
+        const selectors = [`body #search h3`, `body #search h3 > a`];
+        for (const selector of selectors) {
+            for (const h3 of document.querySelectorAll(selector)) {
+                if (h3?.tagName === 'H3') {
+                    if (!!h3.parentElement?.getAttribute("target")) continue;
+                    h3.parentElement?.setAttribute("target", "_blank");
+                }
+                if (h3?.tagName === 'A') {
+                    if (!!h3?.getAttribute("target")) continue;
+                    h3?.setAttribute("target", "_blank");
+                }
+            }
+        }
+    }
+
+    const cronJob = () => {
+        setNewTab();
+    }
+
+
+    setInterval(cronJob, 1000)
+})();
